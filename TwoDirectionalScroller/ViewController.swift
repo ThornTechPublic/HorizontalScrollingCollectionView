3f45a337ae33b06018fb2c51a4b2ee6dbabb8b3a
Robert Chen <robert.chen@thorntechnologies.com>
2015-07-11 16:55:30 -0400

    Initial Commit

---
+++ TwoDirectionalScroller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoDirectionalScroller
+//
+//  Created by Robert Chen on 7/11/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
